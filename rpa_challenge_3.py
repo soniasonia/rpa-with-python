e2f13b9b3b4f793971771235fe3ba726729fa51b
sonia <pawlaksonia@gmail.com>
2020-02-25 19:32:02 +0100

    Extracting reviews from web

---
+++ rpa_challenge_3.py
@@ -0,0 +1,34 @@
+import openpyxl
+from selenium import webdriver
+
+INPUT = "challenge.xlsx"
+URL = "http://www.rpachallenge.com/movieSearch"
+
+
+def get_data():
+    driver = webdriver.Firefox()
+    driver.get(URL)
+    js = '''
+        var btns = document.getElementsByTagName("button");
+        for (e of btns){    
+            if (e.innerText == "GET POPULAR MOVIES"){
+                e.click();
+            }
+        }
+
+        var review_list = [];
+            var icons = document.getElementsByClassName("instructionsDiv")[0].getElementsByTagName("i");
+            for (icon of icons){
+                if (icon.textContent == "comment"){
+                  icon.click();  
+                  var reviews = document.getElementsByClassName("modal-content")[0].getElementsByClassName("card");
+                  for (review of reviews) {        
+                    review_list.append(review.getElementsByTagName("p")[0].innerText);
+                  }
+                };
+            }
+    '''
+    return driver.execute_script(js)
+
+seba = get_data()
+print(seba)
